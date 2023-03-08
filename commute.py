47e620a6a99cd6676628eedb3710f4fbd513900e
DAWUN <dawun@lacy.co.kr>
2023-03-08 17:31:05 +0900

    background server 동작 option 추가

--- commute.py
+++ commute.py
@@ -245,6 +245,7 @@ if __name__ == "__main__":
     # 크로미움 설정
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('window-size=1920x1080')
     chrome_options.add_argument("disable-gpu")
     chrome_options.add_argument('lang=ko_KR')
