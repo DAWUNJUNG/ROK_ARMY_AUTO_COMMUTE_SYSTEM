c18c349b2a8144b22cfee479fd22df69abb68de9
dawun <dawun@lacy.co.kr>
2023-09-09 18:00:16 +0900

    test

--- commute.py
+++ commute.py
@@ -34,13 +34,14 @@ class AutoCommute:
 
         # 크로미움 설정
         self.chrome_options = webdriver.ChromeOptions()
-        self.chrome_options.add_argument('headless')
-        self.chrome_options.add_argument('--no-sandbox')
+        self.chrome_options.add_argument("--incognito")
+        self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--disable-setuid-sandbox")
+        self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.chrome_options.add_argument('window-size=1920x1080')
         self.chrome_options.add_argument("disable-gpu")
         self.chrome_options.add_argument('lang=ko_KR')
-        self.chrome_options.add_argument("--single-process")
-        self.chrome_options.add_argument("--disable-dev-shm-usage")
         self.chrome_options.add_argument('ignore-certificate-errors')  # SSL 관련 오류 무시
         self.chrome_options.add_argument('ignore-ssl-errors')  # SSL 관련 오류 무시
 
