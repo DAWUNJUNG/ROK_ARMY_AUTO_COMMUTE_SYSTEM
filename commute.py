d533234463fc288e54fd37f600be3f09665b2f3f
dawun <dawun@lacy.co.kr>
2023-09-11 19:24:37 +0900

    test

--- commute.py
+++ commute.py
@@ -37,6 +37,8 @@ class AutoCommute:
         self.chrome_options.add_argument("--incognito")
         self.chrome_options.add_argument("--headless")
         self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--disable-extensions")
+        self.chrome_options.add_argument("--disable-gpu")
         self.chrome_options.add_argument("--disable-setuid-sandbox")
         self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.chrome_options.add_argument('window-size=1920x1080')
