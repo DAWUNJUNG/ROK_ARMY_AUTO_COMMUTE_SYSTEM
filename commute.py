5751c5064fb5d5648854a143b0ecc5d2fcfd82d7
DAWUN <dawun@lacy.co.kr>
2023-09-12 13:24:14 +0900

    test

--- commute.py
+++ commute.py
@@ -54,10 +54,10 @@ class AutoCommute:
                             encoding="UTF-8")
 
         # 크롬 설치
-        chromedriver_autoinstaller.install()
+        # chromedriver_autoinstaller.install()
 
         # 설정 정보 할당
-        self.browser = wired_webdriver.Chrome(seleniumwire_options=self.options, chrome_options=self.chrome_options)
+        self.browser = wired_webdriver.Chrome(executable_path=str(os.environ.get('CHROME_DRIVER_PATH')), seleniumwire_options=self.options, chrome_options=self.chrome_options)
         self.log("근태 기록 자동화 시작\n" +
                  f"프로세스 시작 시간 : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n" +
                  "Made By Dawun (github : https://github.com/DAWUNJUNG)\n")
