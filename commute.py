f7c285b60f47163cc550bb28239ed9bf9940a7a6
DAWUN <dawun@lacy.co.kr>
2023-09-12 11:23:37 +0900

    Revert "test"
    This reverts commit 1ac74d023890830dc38c74138b9213019e72487d.

--- commute.py
+++ commute.py
@@ -3,7 +3,7 @@ from dotenv import find_dotenv, load_dotenv
 import os
 from selenium import webdriver
 from seleniumwire import webdriver as wired_webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
@@ -57,7 +57,7 @@ class AutoCommute:
                             encoding="UTF-8")
 
         # 설정 정보 할당
-        self.browser = wired_webdriver.Chrome(service=Service(ChromeDriverManager(log_level=40).install()), seleniumwire_options=self.options, chrome_options=self.chrome_options)
+        self.browser = wired_webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), seleniumwire_options=self.options, chrome_options=self.chrome_options)
         self.log("근태 기록 자동화 시작\n" +
                  f"프로세스 시작 시간 : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n" +
                  "Made By Dawun (github : https://github.com/DAWUNJUNG)\n")
