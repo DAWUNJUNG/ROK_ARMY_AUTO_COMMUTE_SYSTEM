869f73f8345c23d340560d8bce4a1a393a0e79e4
dawun <dawun@lacy.co.kr>
2023-09-17 23:19:43 +0900

    test

--- commute.py
+++ commute.py
@@ -54,7 +54,7 @@ class AutoCommute:
 
         # 크롬 설치
         chromedriver_autoinstaller.install()
-        service = Service('chromedriver')
+        service = Service(executable_path='chromedriver')
 
         # 설정 정보 할당
         self.browser = webdriver.Chrome(service=service, seleniumwire_options=self.options, options=self.chrome_options)
